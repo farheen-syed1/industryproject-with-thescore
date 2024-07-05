546a416f7ce85caefaaedd94e8b6de5b79005b73
jacquikeating <j.keating@live.ca>
2024-07-05 10:45:33 -0400

    Built hero section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import Header from '../../components/Header/Header.jsx'
-import Footer from "../../components/Footer/Footer.jsx"
+import React from "react";
+import Header from "../../components/Header/Header.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
+import "./HomePage.scss";
 
 export default function HomePage() {
-  return (<>
-  <Header />
-  <main>
-    <h1>Report: Warriors complete sign-and-trade with 76ers for Hield</h1>
-    <h2>Latest</h2>
-    <h3>Report: Prescott nursing foot sprain</h3>
-    <p>40m ago • 1m read</p>
-  </main>
-  <Footer />
-    </>)
+  return (
+    <>
+      <Header />
+      <section className="hero">
+        <img
+          className="hero__img"
+          src="/src/assets/football-chris-chow-unsplash.jpg"
+          alt=""
+        />
+        <h1 className="hero__text">
+          Your Voice, Your Choice: <br /> Only on TheScore!
+          {/* Be the MVP: <br /> Vote Now on TheScore! */}
+        </h1>
+      </section>
+      <main>
+        <h2>More Text</h2>
+        <h3>Will go here</h3>
+        <p>40m ago • 1m read</p>
+      </main>
+      <Footer />
+    </>
+  );
 }
