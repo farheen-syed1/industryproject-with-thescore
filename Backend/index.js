eee42e613e2a52b6a8a9263b9ba3cd6d0443f679
BOYI <user@BOYIs-MacBook-Air.local>
2024-07-05 14:42:14 -0400

    Added option cards

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
-import {
-  fileURLToPath
-} from 'url';
+import { fileURLToPath } from 'url';
+import cors from 'cors'; // Import CORS
 import nbaRoutes from './routes/nba.js';
 import soccerRoutes from './routes/soccer.js';
 import nflRoutes from './routes/nfl.js';
@@ -13,10 +12,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const __filename = fileURLToPath(
-  import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(cors()); // Enable CORS
 app.use('/images', express.static(path.join(__dirname, '../src/assets/images')));
 
 app.use('/api/nba', nbaRoutes);
