f1a557e0dafdcb42e1d6e2a6f177c031ea5131ac
jacquikeating <j.keating@live.ca>
2024-07-04 20:48:25 -0400

    Header styles
    - Built basic header (appearance only, no functionality)

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,12 +3,10 @@ import Header from '../../components/Header/Header.jsx'
 
 export default function HomePage() {
   return (<>
-  <Header/>
-    <div>Join Fan Zone Hub by clicking your sport below:</div>
-    <ul>
-        <li>NFL</li>
-        <li>MLB</li>
-        <li>NHL</li>
-    </ul>
+  <Header />
+    <h1>Report: Warriors complete sign-and-trade with 76ers for Hield</h1>
+    <h2>Latest</h2>
+    <h3>Report: Prescott nursing foot sprain</h3>
+    <p>40m ago • 1m read</p>
     </>)
 }
