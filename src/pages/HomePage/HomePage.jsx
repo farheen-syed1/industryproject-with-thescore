ba7f4d3255ebe612c76e6488d109b2a3b68f5b8b
jacquikeating <j.keating@live.ca>
2024-07-04 21:27:31 -0400

    Built basic footer
    - Built and styled footer component
    - No functionality
    - Added a new section for our content (temporarily named Vote)
    - Currently hardcoded so that Vote is always active. Other sections do not need to be functional for this demo. If we have extra time, we can add the ability to switch sections (filled with dummy content) and have the --active modifier apply accordingly.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../../components/Header/Header.jsx'
+import Footer from "../../components/Footer/Footer.jsx"
 
 export default function HomePage() {
   return (<>
   <Header />
+  <main>
     <h1>Report: Warriors complete sign-and-trade with 76ers for Hield</h1>
     <h2>Latest</h2>
     <h3>Report: Prescott nursing foot sprain</h3>
     <p>40m ago • 1m read</p>
+  </main>
+  <Footer />
     </>)
 }
