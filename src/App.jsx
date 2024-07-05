c4644df2e38161d8bdc8a59a7842d5ef1a619b17
BOYI <user@BOYIs-MacBook-Air.local>
2024-07-05 14:46:32 -0400

    Added the options card and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.scss'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import HomePage from './pages/HomePage/HomePage'
 import FanZonePage from './pages/FanZonePage/FanZonePage'
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/nhl" element={<FanZonePage />} />
-    </Routes>
-    </BrowserRouter>
-
-      </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/fanzone/:sport" element={<FanZonePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
