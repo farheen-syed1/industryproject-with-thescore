019c4d9ea2ce8203be2685f86065ddb7e0541b45
jacquikeating <j.keating@live.ca>
2024-07-05 11:36:17 -0400

    Cards added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -18,10 +18,43 @@ export default function HomePage() {
           {/* Be the MVP: <br /> Vote Now on TheScore! */}
         </h1>
       </section>
-      <main>
-        <h2>More Text</h2>
-        <h3>Will go here</h3>
-        <p>40m ago • 1m read</p>
+      <main className="main">
+        <h2 className="main__headline">
+          We need more text to explain what this actually is but I'm bad at
+          writing
+        </h2>
+        <p className="main__copy">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero, quae
+          molestias facere sequi quibusdam corporis accusamus inventore ea
+          veritatis dolorem.
+        </p>
+
+        <section className="homepage-cards-section">
+          <div className="homepage-card">
+            <img
+              className="homepage-card__icon"
+              src="/src/assets/basketball-icon.svg"
+              alt="Basketball icon"
+            />
+            <h3 className="homepage-card__text">Basketball</h3>
+          </div>
+          <div className="homepage-card">
+            <img
+              className="homepage-card__icon"
+              src="/src/assets/football-icon.svg"
+              alt="Football icon"
+            />
+            <h3 className="homepage-card__text">Football</h3>
+          </div>
+          <div className="homepage-card">
+            <img
+              className="homepage-card__icon"
+              src="/src/assets/soccer-icon.svg"
+              alt="Soccer ball icon"
+            />
+            <h3 className="homepage-card__text">Soccer</h3>
+          </div>
+        </section>
       </main>
       <Footer />
     </>
