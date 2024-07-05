93eead191a7ac3370c869f5c4e0c93b9cbb354ec
Shruti Srinivasan <shruti.srinivasan.87@gmail.com>
2024-07-05 10:37:15 -0400

    Comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,9 +1,19 @@
-import './Comments.scss'
+import "./Comments.scss";
+import avatar from '../../assets/images/avatar.jpg'
+import React from "react";
 
-import React from 'react'
-
-export default function Comments() {
-  return (
-    <div>Comments</div>
-  )
+export default function Comments({comment}) {
+  
+    return (
+      <div className="comment">
+        <div className="comment__avatar"><img className="comment__img" src={avatar}/> </div>
+        <div className="comment__box">
+          <div className="comment__metadata">
+            ${comment.name} - {comment.time}
+          </div>
+          <div className="comment__text">{comment.comment}</div>
+        </div>
+      </div>
+    );
+  
 }
--- src/pages/FanZonePage/FanZonePage.jsx
+++ src/pages/FanZonePage/FanZonePage.jsx
@@ -1,16 +1,26 @@
 import "./FanZonePage.scss";
 import React from "react";
 import Option from "../../components/Option/Option.jsx";
-import Comments from "../../components/Comments/Comments.jsx"
+import Comments from "../../components/Comments/Comments.jsx";
+import commentsData from "../../data/comments.json";
+
 export default function FanZonePage() {
   return (
     <>
-      <div>
-        <h1>Fan Zone</h1>
-        <Option option={"Team 1"} />
-        <Option option={"Team 2"} />
-      </div>
-      <Comments />
+      <main>
+        <div>
+          <h1>Fan Zone</h1>
+          <Option option={"Team 1"} />
+          <Option option={"Team 2"} />
+        </div>
+        {commentsData.map((comment) => {
+          return (
+            <div className="comments__comment" key={comment.name}>
+              <Comments comment={comment} />
+            </div>
+          );
+        })}
+      </main>
     </>
   );
 }
